a154cca527eac5db5a4659222204acdea4f26806
Matthew-Tuazon <matttuazon106@gmail.com>
2018-08-20 18:04:41 -0400

    finished API Call, as well as display. Need to work on truncating weather decimal.

--- Weather-App/ViewController.swift
+++ Weather-App/ViewController.swift
@@ -12,11 +12,15 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var weatherLabel: UILabel!
     
-    @IBOutlet weak var weatherButtonTapped: UIButton!
+    @IBAction func weatherButtonTapped(_ sender: UIButton) {
+        getWeather()
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getWeather()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +28,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func getWeather() {
+    let session = URLSession.shared
+    let weatherURL = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=Bergenfield,us?&units=imperial&APPID={YOUR_API_KEY_HERE}")! //Writes out a URL session
+        let dataTask = session.dataTask(with: weatherURL) { //takes API call
+            (data: Data?, response: URLResponse?, error: Error?) in
+            if let error = error {
+                print("Error:\n\(error)")
+            } else {
+            if let data = data {
+                let dataString = String(data: data, encoding: String.Encoding.utf8) //for easy string print to console
+                
+                    print("All the weather data:\n\(dataString!)")
+                    if let jsonObj = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? NSDictionary {
+                        if let mainDictionary = jsonObj!.value(forKey: "main") as? NSDictionary {
+                            if let temperature = mainDictionary.value(forKey: "temp") {
+                                DispatchQueue.main.async {
+                                    self.weatherLabel.text = "Bergenfield Temperature: \(temperature)°F"
+                                }
+                            }
+                        } else {
+                            print("Error: unable to find temperature in dictionary")
+                        }
+                    } else {
+                        print("Error: unable to convert json data")
+                    }
+                } else {
+                    print("Error: did not receive data")
+                }
+            }
+        }
+        dataTask.resume()
+    }
 }
+        
+        
+        
+
+    
+
 
